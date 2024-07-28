173818c6dd635e64b90cde7e5c2aff456888a15c
birongliu <birongliu2003@gmail.com>
2024-07-28 09:32:04 -0400

    feat: Add form validation to prevent empty submissions

--- assets/js/other.js
+++ assets/js/other.js
@@ -3,6 +3,11 @@ document.getElementById("form-btn").addEventListener("click", e => {
 
     const form = document.getElementById("form-sub")
     let data = new FormData(form)
+    const transformData = [...data.values()]
+    if(transformData.every(k => k === "")) {
+        alert("Please fill in all fields")
+        return;
+    }
     alert("Thank you, your form was submitted")
     form.reset()
 })
